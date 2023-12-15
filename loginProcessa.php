651060a3839768c41a8a439f788d1f99d14be7c6
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-12-14 21:03:53 -0300

    14/12/23

--- loginProcessa.php
+++ loginProcessa.php
@@ -22,21 +22,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['usuario'] = $loginUsuario; // Definindo variável de sessão
             if ($tipoConta === "C") {
                 $_SESSION['tipo'] = $tipoConta;
-                header("location:index.php?msgSucesso=Login bem-sucedido!");
-                
+                header("location: index.php?msgSucesso=Login bem-sucedido!");
+
             } elseif ($tipoConta === "F") {
                 $_SESSION['tipo'] = $tipoConta;
                 header("location: cadastroproduto.php?msgSucesso=Login bem-sucedido!");
-              
+
             } else {
                 header("location:login.php?msgErro=Você não tem permissão para acessar essa conta!");
             }
-            
+
         } else {
             $_SESSION['error'] = "Senha incorreta. Tente novamente.";
             header("location:login.php?msgErro=Senha incorreta. Tente novamente!");
-          
-          
+
+
         }
     } else {
 
@@ -44,14 +44,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Destruir a SESSAO
         session_destroy();
         header("location:login.php?msgErro=Usuário não encontrado. Verifique suas credenciais!");
-        
+
     }
     $stmtLogin->close();
 
 } else {
 
     header("location:login.php?msgErro=Método de requisição inválido!");
-   
+
 }
 $conn->close();
 
